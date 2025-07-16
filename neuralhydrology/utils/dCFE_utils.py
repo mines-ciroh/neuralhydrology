c46ef3b7ccec6c0a647fe706d382f0a8d33473a4
Ziyu Li <70241363+ziyuli22@users.noreply.github.com>
2025-07-16 11:46:05 +0200

    Modified DCFE_utils.py to use keys only to retrieve items

--- neuralhydrology/utils/dCFE_utils.py
+++ neuralhydrology/utils/dCFE_utils.py
@@ -26,22 +26,14 @@ def get_dcfe_params(cfg):
 
     cfe_param_dir = cfg.param_dir
     calibrated_params_dir = cfg.calibrated_params_path
-    # get all the basin ids as strings, now only from train_basin_file but in other code it was {period}_basin_file
-    # where period is train, val, test etc
+    
+    # --- get all the basin ids as strings ---
     basins = utils.load_basin_file(getattr(cfg, "train_basin_file"))
-    # empty dataframe to store the parameters, basin ids as index
-    # df = pd.DataFrame(index=basins, columns=["soil_params", "basinCharacteristics"])
-
+    
     col_keys = keys['soil'] + keys['basin_characteristics']
     df = pd.DataFrame(index=basins, columns=col_keys)
 
-    ####
-
-    # not sure why below is here, comment out for now
-    # if basin_id[0] != "0":
-    #    basin_id = "0" + basin_id
-
-    # iterate thru each basin id
+    # --- iterate thru each basin id to get params ---
     for basin_id in basins:
         cfe_param_file_path = cfe_param_dir / (basin_id + "_bmi_config_cfe_pass.txt")
         with open(cfe_param_file_path, "r") as f:
@@ -56,91 +48,51 @@ def get_dcfe_params(cfg):
             except:
                 matches[match[0]] = [float(x) for x in match[1].split(",")]
 
-        # TODO: Write the code below as a single function call.
-        soil_params = {
-            "depth": torch.tensor(matches["soil_params.depth"], dtype=torch.float32),
-            "bb": torch.tensor(matches["soil_params.b"], dtype=torch.float32),
-            "satdk": torch.tensor(matches["soil_params.satdk"], dtype=torch.float32),
-            "satpsi": torch.tensor(matches["soil_params.satpsi"], dtype=torch.float32),
-            "slop": torch.tensor(matches["soil_params.slop"], dtype=torch.float32),
-            "smcmax": torch.tensor(matches["soil_params.smcmax"], dtype=torch.float32),
-            "wltsmc": torch.tensor(matches["soil_params.wltsmc"], dtype=torch.float32),
-            "D": torch.tensor(2.0, dtype=torch.float32),
-            "mult": torch.tensor(1.0, dtype=torch.float32),
-        }
-        # TODO: Write the code below as a single function call.
-        basinCharacteristics = {
-            "catchment_area_km2": torch.tensor(111.11, dtype=torch.float32),
-            "refkdt": torch.tensor(matches["refkdt"], dtype=torch.float32),
-            "max_gw_storage": torch.tensor(matches["max_gw_storage"], dtype=torch.float32),
-            "expon": torch.tensor(matches["expon"], dtype=torch.float32),
-            "Cgw": torch.tensor(matches["Cgw"], dtype=torch.float32),
-            "alpha_fc": torch.tensor(matches["alpha_fc"], dtype=torch.float32),
-            "K_nash": torch.tensor(matches["K_nash"], dtype=torch.float32),
-            "K_lf": torch.tensor(matches["K_lf"], dtype=torch.float32),
-            "nash_storage": torch.tensor(matches["nash_storage"], dtype=torch.float32),
-            "giuh_ordinates": torch.tensor(matches["giuh_ordinates"], dtype=torch.float32),
-        }
+        soil_params = {}
+        for k in keys["soil"]:
+            match_key = "b" if k == "bb" else k
+            if k == "D":
+                soil_params[k] = torch.tensor(2.0, dtype=torch.float32)
+            elif k == "mult":
+                soil_params[k] = torch.tensor(1.0, dtype=torch.float32)
+            else:
+                soil_params[k] = torch.tensor(matches[f"soil_params.{match_key}"], dtype=torch.float32)
 
-        # Update parameters from JSON file in calibrated_params_dir
+        basinCharacteristics = {}
+        for k in keys["basin_characteristics"]:
+            if k == "catchment_area_km2":
+                basinCharacteristics[k] = torch.tensor(111.11, dtype=torch.float32)
+            else:
+                basinCharacteristics[k] = torch.tensor(matches[k], dtype=torch.float32)
 
-        # TODO: Ensure that all basins have corresponding json files. Add a try/except block to handle missing files.
+        # --- Update parameters from JSON file in calibrated_params_dir ---
+        # TODO: Maybe add an option to use the default parameters, instead of the calibrated ones?
         json_file_path = calibrated_params_dir / f"cat_{basin_id}_testrun_results.json"
         if json_file_path.exists():
             with open(json_file_path, "r") as file:
                 data = json.load(file)
                 best_params = data.get("best_params", {})
+                
+                for k in best_params.keys():
+                    match_key = "refkdt" if k == "scheme" else k
+                    if match_key in keys["soil"]:
+                        temp_value = best_params.get(match_key, soil_params[match_key])
+                        soil_params[match_key] = temp_value.clone().detach() if isinstance(temp_value, torch.Tensor) \
+                            else torch.tensor(temp_value, dtype=torch.float32)
+                    elif match_key in keys["basin_characteristics"]:
+                        temp_value = best_params.get(match_key, basinCharacteristics[match_key])
+                        basinCharacteristics[match_key] = temp_value.clone().detach() if isinstance(temp_value, torch.Tensor) \
+                            else torch.tensor(temp_value, dtype=torch.float32)
+                    else:
+                        print(f"[warn] Parameter {k} not recognized in keys, skipping update for basin {basin_id}.")
+        else:
+            print(f"[warn] JSON file not found for basin {basin_id}, using default parameters.")
 
-                # Update the parameters in soil_params and basinCharacteristics
-                soil_params["bb"] = torch.tensor(
-                    best_params.get("bb", soil_params["bb"].item()),
-                    dtype=torch.float32,
-                )
-                soil_params["smcmax"] = torch.tensor(
-                    best_params.get("smcmax", soil_params["smcmax"].item()),
-                    dtype=torch.float32,
-                )
-                soil_params["satdk"] = torch.tensor(
-                    best_params.get("satdk", soil_params["satdk"].item()),
-                    dtype=torch.float32,
-                )
-                soil_params["slop"] = torch.tensor(
-                    best_params.get("slop", soil_params["slop"].item()),
-                    dtype=torch.float32,
-                )
-                basinCharacteristics["max_gw_storage"] = torch.tensor(
-                    best_params.get("max_gw_storage", basinCharacteristics["max_gw_storage"].item()),
-                    dtype=torch.float32,
-                )
-                basinCharacteristics["expon"] = torch.tensor(
-                    best_params.get("expon", basinCharacteristics["expon"].item()),
-                    dtype=torch.float32,
-                )
-                basinCharacteristics["Cgw"] = torch.tensor(
-                    best_params.get("Cgw", basinCharacteristics["Cgw"].item()),
-                    dtype=torch.float32,
-                )
-                basinCharacteristics["K_lf"] = torch.tensor(
-                    best_params.get("K_lf", basinCharacteristics["K_lf"].item()),
-                    dtype=torch.float32,
-                )
-                basinCharacteristics["K_nash"] = torch.tensor(
-                    best_params.get("K_nash", basinCharacteristics["K_nash"].item()),
-                    dtype=torch.float32,
-                )
-                basinCharacteristics["refkdt"] = torch.tensor(
-                    best_params.get("scheme", basinCharacteristics["refkdt"].item()),
-                    dtype=torch.float32,
-                )
-
-        # --- 4) Unpack into DataFrame row ---
+        # --- Unpack into DataFrame row ---
         for item in keys['soil']:
-        # for item in ["depth", "bb", "satdk", "satpsi", "slop", "smcmax", "wltsmc", "D", "mult"]:
             df.at[basin_id, item] = soil_params[item]
         
         for item in keys['basin_characteristics']:
-        # for item in ["catchment_area_km2", "refkdt", "max_gw_storage", "expon",
-        #             "Cgw", "alpha_fc", "K_nash", "K_lf", "nash_storage", "giuh_ordinates"]:
             df.at[basin_id, item] = basinCharacteristics[item]
 
     return df
@@ -300,28 +252,40 @@ def filter_basins_all_param_files(
 
 keys = {
     "basin_characteristics": [
-    "catchment_area_km2",
-    "refkdt",
-    "max_gw_storage",
-    "expon",
-    "Cgw",
-    "alpha_fc",
-    "K_nash",
-    "K_lf",
-    "nash_storage",
-    "giuh_ordinates",
-    ],
+        "catchment_area_km2",
+        "refkdt",
+        "max_gw_storage",
+        "expon",
+        "Cgw",
+        "alpha_fc",
+        "K_nash",
+        "K_lf",
+        "nash_storage",
+        "giuh_ordinates",
+        ],
     "soil": [
-    "depth",
-    "bb",
-    "satdk",
-    "satpsi",
-    "slop",
-    "smcmax",
-    "wltsmc",
-    "D",
-    "mult",
-    ]
+        "depth",
+        "bb",
+        "satdk",
+        "satpsi",
+        "slop",
+        "smcmax",
+        "wltsmc",
+        "D",
+        "mult",
+        ],
+    "study_calibrated_params": [
+        "bb",
+        "smcmax",
+        "satdk",
+        "slop",
+        "max_gw_storage",
+        "expon",
+        "Cgw",
+        "K_lf",
+        "K_nash",
+        "refkdt",
+        ],
     }
 
 # used in CFE modules
